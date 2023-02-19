5d68f99baa31c3ed8de03165eac426593d7a2964
eve_li <840745490@qq.com>
2023-02-19 22:07:46 +0800

    test 17 14 and 9 key accuracy.
    Signed-off-by: eve_li <840745490@qq.com>

--- eve_script/phone_analysis.py
+++ eve_script/phone_analysis.py
@@ -25,3 +25,43 @@ with open('D:/lkg/1. pinyin/5. program/words_lib/hanzipinyin/out/normal_phone.tx
         f.write(item[0] + '\n')
 print(len(phone_dict))
 print(len(normal_phone_dict))
+# 要用14或者九键的替换查看的话，用文件17_14_9.txt文件中的phone字段替换如下部分
+with open('D:/lkg/1. pinyin/5. program/words_lib/hanzipinyin/out/17key_phone_417.txt', 'w', encoding='utf-8') as f:
+    phone_key17_dic = {}
+    for item in phone_dict:
+        phone = str(item[0])
+        phone = phone.replace('w','q')
+        phone = phone.replace('r','e')
+        phone = phone.replace('u','y')
+        phone = phone.replace('p','o')
+        phone = phone.replace('s','a')
+        phone = phone.replace('f','d')
+        phone = phone.replace('j','h')
+        phone = phone.replace('x','z')
+        phone = phone.replace('v','c')
+        phone_key17_dic[phone] = 1
+    for item in phone_key17_dic:
+        f.writelines(item[0])
+print(f'所有音节：{len(phone_dict)}')
+print(f'17key：{len(phone_key17_dic)}')
+print(f'百分比：{len(phone_key17_dic)/len(phone_dict)}')
+
+with open('D:/lkg/1. pinyin/5. program/words_lib/hanzipinyin/out/17key_phone_417.txt', 'w', encoding='utf-8') as f:
+    phone_key17_dic = {}
+    for item in normal_phone_dict:
+        phone = str(item[0])
+        phone = phone.replace('w','q')
+        phone = phone.replace('r','e')
+        phone = phone.replace('u','y')
+        phone = phone.replace('p','o')
+        phone = phone.replace('s','a')
+        phone = phone.replace('f','d')
+        phone = phone.replace('j','h')
+        phone = phone.replace('x','z')
+        phone = phone.replace('v','c')
+        phone_key17_dic[phone] = 1
+    for item in phone_key17_dic:
+        f.writelines(item[0])
+print(f'常用音节：{len(normal_phone_dict)}')
+print(f'17key：{len(phone_key17_dic)}')
+print(f'百分比：{len(phone_key17_dic)/len(normal_phone_dict)}')
